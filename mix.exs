0bfd0035cee1e079c3aa6f1ee6292ed4c3f7fe80
Ian Luites <IanLuites@users.noreply.github.com>
2020-03-17 23:39:21 +0100

    Release v0.3.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Buckaroo.MixProject do
     [
       app: :buckaroo,
       description: "Simple `:cowboy` (v2) webserver with support for SSE and WebSockets.",
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
